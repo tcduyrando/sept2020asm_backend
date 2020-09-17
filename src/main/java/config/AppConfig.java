7040071601f8d4b3d7b264363f1501e4e3e30360
tcduyrando <67744348+tcduyrando@users.noreply.github.com>
2020-09-18 05:11:31 +0700

    Add files via upload

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -48,9 +48,9 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/SEPT2020");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setUrl("postgres://ldssmhjwwwmpri:b5e9c9ff23b95d248650d0044e5342f95fc64c177b7e6d624aaa301a9d1dbf23@ec2-52-200-134-180.compute-1.amazonaws.com:5432/de2rb9845gu4g9");
+        dataSource.setUsername("ldssmhjwwwmpri");
+        dataSource.setPassword("b5e9c9ff23b95d248650d0044e5342f95fc64c177b7e6d624aaa301a9d1dbf23");
 
         sessionFactoryBean.setDataSource(dataSource);
         sessionFactoryBean.setHibernateProperties(properties);
